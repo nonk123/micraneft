9c44e2993fd02429f230f1266d7b589097bd35f7
nonk123 <Sergeyii321@gmail.com>
2021-02-22 19:59:25 +0300

    Make the game run smooth as butter
    Like, seriously. 60 FPS? No problem. 1080p? All's good.

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
+#include <sys\timeb.h>
 
 #include <windows.h>
 
 #include "world.h"
 #include "console.h"
 
-#define FPS 24
+#define PHYSICS_FPS 30
+#define MAX_FPS 60
 
 typedef struct controls_t {
   int left, right, up;
 } controls_t;
 
-void accept_input(controls_t* controls)
+void accept_input(controls_t* controls, entity_t* player)
 {
   int i = 0;
 
   key_event_t* events = receive_key_events();
   key_event_t event = events[i++];
+
+  const double walking_speed = 12.0;
+  const double jump_power = 8.0;
 
   while (!event.empty)
     {
@@ -43,6 +49,12 @@ void accept_input(controls_t* controls)
     }
 
   free(events);
+
+  if (controls->up && player->is_on_floor)
+    player->vy = jump_power;
+
+  player->vx = controls->right * walking_speed;
+  player->vx -= controls->left * walking_speed;
 }
 
 int main()
@@ -52,25 +64,53 @@ int main()
 
   controls_t controls = {0};
 
-  const double walking_speed = 12.0;
-  const double jump_power = 8.0;
+  const double min_delta = 1.0 / MAX_FPS;
+  const double physics_delta = 1.0 / PHYSICS_FPS;
+
+  double last_frame = 0.0, current_frame;
+  double second = 0.0;
+
+  int actual_fps = 0, current_fps = 0;
 
   for (;;)
     {
+      struct timeb start, end;
+
+      ftime(&start);
+
       disable_cursor();
 
-      accept_input(&controls);
+      accept_input(&controls, player);
 
-      if (controls.up && player->is_on_floor)
-        player->vy = jump_power;
-
-      player->vx = controls.right * walking_speed;
-      player->vx -= controls.left * walking_speed;
-
-      physics_tick(&world, 1.0 / FPS);
+      physics_tick(&world, physics_delta);
       print_world(&world, player->ix, player->iy + 1);
 
-      Sleep(1000 / FPS);
+      move_cursor_to(0, 0);
+      printf("FPS: %d", current_fps);
+
+      ftime(&end);
+
+      current_frame = (end.time - start.time) + 0.001 * (end.millitm - start.millitm);
+
+      if (current_frame < min_delta)
+        {
+          /* Sleep to reach the desired FPS. */
+          Sleep(1000 * (min_delta - current_frame));
+          second += min_delta;
+        }
+      else
+        second += current_frame;
+
+      actual_fps++;
+
+      if (second >= 1.0)
+        {
+          second -= 1.0;
+          current_fps = actual_fps;
+          actual_fps = 0;
+        }
+
+      last_frame = current_frame;
     }
 
   return 0;
--- print.c
+++ print.c
@@ -45,15 +45,24 @@ void print_world(world_t* world, int center_x, int center_y)
   int x0, y0;
 
   static char* extra_buffer = NULL;
+  static int buffer_size = 0;
+  int old_buffer_size;
 
   tile_t* copy = copy_world(world);
 
+  /* Magic values to trigger the check below. */
+  int cursor_x = -2, cursor_y = 0;
+
   get_console_window_size(&rows, &cols);
 
-  if (extra_buffer == NULL)
-    extra_buffer = calloc(rows * cols, sizeof(char));
-  else
-    extra_buffer = realloc(extra_buffer, rows * cols * sizeof(char));
+  old_buffer_size = buffer_size;
+  buffer_size = rows * cols * sizeof(char);
+
+  extra_buffer = realloc(extra_buffer, buffer_size);
+
+  /* Redraw when the window size changes. */
+  if (old_buffer_size != buffer_size)
+    memset(extra_buffer, 0, buffer_size);
 
   x0 = center_x - cols / 2;
   y0 = center_y - rows / 2;
@@ -65,27 +74,28 @@ void print_world(world_t* world, int center_x, int center_y)
       for (x = 0; x < cols; x++)
         {
           char* in_buffer = &extra_buffer[rows * x + y];
+          char draw = 0; /* draw nothing by default */
 
           int tile_x = x0 + x;
 
           if (tile_y >= 0 && tile_x >= 0
               && tile_y < world->height && tile_x < world->width)
-            {
-              char ours = get_tile(copy, tile_x, tile_y, world->width, world->height)->displayed_as;
-
-              if (ours != *in_buffer)
-                {
-                  /* The entire world is upside-down. */
-                  move_cursor_to(x, rows - 1 - y);
-                  putchar(ours);
-                  *in_buffer = ours;
-                }
-            }
+            draw = get_tile(copy, tile_x, tile_y, world->width, world->height)->displayed_as;
           else
+            draw = ' ';
+
+          if (draw && draw != *in_buffer)
             {
-              move_cursor_to(x, rows - 1 - y);
-              putchar(' ');
-              *in_buffer = ' ';
+              /* putchar moves the cursor right anyways. */
+              if (cursor_y != y || x - cursor_x > 1)
+                /* The entire world is upside-down. */
+                move_cursor_to(x, rows - 1 - y);
+
+              putchar(draw);
+              *in_buffer = draw;
+
+              cursor_x = x;
+              cursor_y = y;
             }
         }
     }
